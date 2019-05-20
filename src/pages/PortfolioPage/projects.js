367eee3ba6e5ca2c2b1fb094324a8f5c594f4b0b
Tyrel Chambers <tychambers3@gmail.com>
2019-05-20 11:12:28 -0400

    add new project, remove incomplete

--- src/pages/PortfolioPage/projects.js
+++ src/pages/PortfolioPage/projects.js
@@ -1,26 +1,16 @@
 const projects = [
   {
     "logo": '',
-    "title": "Revel",
-    "status": "In Development",
-    "purpose": "Revel was made because I believed there was a need for an application that helps you find long-term relationships without breaking the bank, and by giving you valuable features in return. I had this idea for a while, but never really acted upon it until I accidently purchased 3 months of Christian Mingle at $130 CAD. With this unfortunate purchase, I was able view more than one profile picture, and send one-way messages. I strongly believe Revel will solve a need and help young people find someone to settle with. As the slogan says, 'When you want someting more than casual'.",
+    "title": "Stories After Midnight",
+    "status": "Completed; Improving.",
+    "purpose": "I built storiesaftermidnight.com for my youtube channel and to have a place where I can advertise my channel. I needed a place to advertise the Humble Bundle's that were released instead of posting them on my social media all the time. This website will also give me a way to showcase more stuff than I could do on my channel itself.",
     "screenshots": [
-      "revel-index.png",
-      "revel-signup.png"
     ],
-    "links": []
-  },
-  {
-    "logo": '../../assets/brain.svg',
-    "title": "Edisco",
-    "status": "In Development",
-    "purpose": "Edisco was created to help bring a solid educational foundation to everyone. By helping teachers share their curriculum with their past, present, and even future students, we can help students achieve success in our own way. By allowing teachers and students to connect online, in an open platform, we can help facilitate collaborative learning.",
-    "screenshots": [],
-    "links": []
+    "links": ["https://storiesaftermidnight.com"]
   },
   {
     "title": "Reddex",
-    "status": "Complete, but improving",
+    "status": "Complete; Improving.",
     "purpose": "Reddex is makes use of Reddit's API to pull up to 1000 posts from whatever subreddit you choose, using various subcategories. It features basic filtering by upvotes and keywords. The main reason for creating Reddex was to speed up my story discovery by delivering me all the created stories, and allowing me to sort them out based on my preferences.",
     "links":["https://reddex.app"],
     "screenshots": []
