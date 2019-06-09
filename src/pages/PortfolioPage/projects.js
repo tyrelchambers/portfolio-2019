d16c4bcfe630a7c5221c9961cf16dd856e5bfe7e
Tyrel Chambers <tychambers3@gmail.com>
2019-06-09 08:23:17 -0400

    add southern cannibal

--- src/pages/PortfolioPage/projects.js
+++ src/pages/PortfolioPage/projects.js
@@ -14,6 +14,13 @@ const projects = [
     "purpose": "Reddex is makes use of Reddit's API to pull up to 1000 posts from whatever subreddit you choose, using various subcategories. It features basic filtering by upvotes and keywords. The main reason for creating Reddex was to speed up my story discovery by delivering me all the created stories, and allowing me to sort them out based on my preferences.",
     "links":["https://reddex.app"],
     "screenshots": []
+  },
+  {
+    "title": "Southern Cannibal",
+    "status": "Complete.",
+    "purpose": "This site was built for a friend in the horror narrator community. It's purpose is to showcase aspects of his brand, that he can't necessarily show on his channel, such as social links, forms, and such. It's a place to hold everything about Southern Cannibal.",
+    "links":["https://southerncannibal.com"],
+    "screenshots": []
   }
 ];
 
